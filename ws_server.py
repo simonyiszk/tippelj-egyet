2c0f42747c8021a563c7a64d5b4d4ea2bf9e8f4f
Kiss Ádám <kiss.adam@simonyi.bme.hu>
2020-04-18 11:37:44 +0200

    Remove meaningless token for clients

--- ws_server.py
+++ ws_server.py
@@ -16,16 +16,15 @@ async def server(websocket, path):
 
     # path.splot('/')[1] == ws
     token = path.split('/')[2] 
-    user  = path.split('/')[3] 
 
     if token not in sessions:
-        sessions[token] = dict()
-    if user not in sessions[token]:
-        sessions[token][user] = websocket
+        sessions[token] = []
+    if websocket not in sessions[token]:
+        sessions[token].append(websocket)
 
     try:
         async for command in websocket:
-            print(f"Got command: {command} from {user} in session {token}")
+            print(f"Got command in session {token}")
 
             if command == 'UN':
                new_images = random.sample(images, k=3)
@@ -38,14 +37,20 @@ async def server(websocket, path):
                         }
                 }
                for i in sessions[token]:
-                   await sessions[token][i].send(json.dumps(ret))
+                   await i.send(json.dumps(ret))
+    except websockets.exceptions.ConnectionClosedError:
+        print(f"Connection closed in {token}")
+    except:
+        print(f"Unknown exception in {token}")
     finally:
         try:
-            del sessions[token][user]
+            sessions[token].remove(websocket)
             if len(sessions[token]) == 0:
                 del sessions[token]
         except KeyError:
-            pass
+            print(f"Unable to free session, KeyError {token}")
+        except:
+            print(f"Unable to free session, unknown error in {token}")
 
 
 start_server = websockets.serve(server, "0.0.0.0", 8765)
